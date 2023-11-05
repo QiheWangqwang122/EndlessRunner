b9569f7fd5e1856a9b6a8abeb49e68e6b8967ae1
qwang122 <1157615907@qq.com>
2023-11-05 01:35:30 -0800

    new

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,33 +5,33 @@ class Play extends Phaser.Scene {
 
     preload() {
         // Load the player spritesheet where each frame is, for example, 32 pixels by 48 pixels.
-        this.load.spritesheet('player', './assets/PlayerRunning.png', { frameWidth: 32, frameHeight: 48 ,startFrame: 0, endFrame: 4});
+        this.load.spritesheet('player', './assets/PlayerRunning.png', { frameWidth: 48, frameHeight: 32 ,startFrame: 0, endFrame: 4});
         this.load.image('ground', './assets/Ground.png');
+        //this.physics.startSystem(Phaser.Physics.ARCADE)
     }
 
     create() {
         const ground = this.physics.add.staticGroup();
-
-        // Create the ground sprite at position (400, 568)
+        //this.physics.add.staticGroup();
+        
         ground.create(400, 568, 'ground').setScale(2).refreshBody();
         
         // Create the player sprite at position (100, 450)
         // Make the player collide with the ground
-        this.physics.add.collider(this.pixelPlayer, ground);
+        
         // Create the animation for standing, assuming the first frame (index 0) is for standing
 
         this.keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         this.keyRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-        this.keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-        this.pixelPlayer = new Pixelplayer(this, 100, 450, 'player', {
+        this.keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        
+        this.pixelPlayer = this.physics.add.existing(new PixelPlayer(this, 400, 450, 'player', {
             keyLeft: this.keyLeft,
             keyRight: this.keyRight,
             keyUP: this.keyUP
-          });
-        this.pixelPlayer.setBounce(0.2);
-        this.pixelPlayer.setCollideWorldBounds(true);
-
-        //this.pixelPlayer.anims.play('stand');
+        }).setOrigin(0.5,0.5));
+        this.pixelPlayer.setCollideWorldBounds(true); 
+        this.physics.add.collider(this.pixelPlayer, ground);
 
         
     }
